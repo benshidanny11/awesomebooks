2bef30ff097b70e43e2eae1a4638a2087415a99e
Niko Escobal <niko.escobal@gmail.com>
2021-09-13 22:33:56 +0800

    Add dev dependencies for Tailwind setup

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://awesomebooks/./src/index.js?");
+eval("//const books = [];\n\ndocument.getElementById('addbook').addEventListener('click', () => {\n  const title = document.getElementById('title').value;\n  const author = document.getElementById('author').value;\n\n  const book = { id: new Date(), title, author };\n  //books.push(book);\n\n\n\n  const tempBooks=[];\n\n  if(localStorage.getItem('booksData') !== null) {\n   tempBooks = JSON.parse(booksData);\n\n   tempBooks.push(book);\n   \n   const convertedBooks = JSON.stringify(tempBooks);\n   localStorage.setItem('booksData', convertedBooks);\n   window.location.reload();\n  }else {\n\n    tempBooks.push(book);\n    \n    const convertedBooks = JSON.stringify(tempBooks);\n    localStorage.setItem('booksData', convertedBooks);\n    window.location.reload();\n   }\n\n \n});\n\nwindow.addEventListener('load', () => {\n  const booksData = localStorage.getItem('booksData');\n  const convertedBooks = JSON.parse(booksData);\n  if (convertedBooks.length === 0) {\n    document.getElementById('bookstatus').innerHTML = 'No books added';\n  } else {\n    let html = '';\n    convertedBooks.forEach((book) => {\n      html += `<article>\n     <h2>${book.title}</h2>\n     <h2>${book.author}</h2>\n     <button data-book-id=\"${book.id}\">Remove</button>\n     <hr />\n   </article></br>`;\n    });\n    document.getElementById('bookstatus').innerHTML = '';\n    document.getElementById('bookslist').innerHTML = html;\n  }\n});\n\n\n//# sourceURL=webpack://awesomebooks/./src/index.js?");
 
 /***/ })
 
