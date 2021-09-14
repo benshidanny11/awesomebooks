74c8650dc22d27b11bb5af468e1b660421f177fc
Danny Benshi <benshidanny11@gmail.com>
2021-09-14 13:14:27 +0200

    Implement classes feature

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 let tempBooks = [];
 
 class Book {
-  constructor(bookId, bookTitle, bookAuthor) {
-    this.bookId = bookId
-    this.bookTitle = bookTitle
-    this.bookAuthor = bookAuthor
+  constructor(id = null, title = null, author = null) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
   }
+
   addBook = () => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
-    const book = {
-      id: new Date().getTime(),
-      title,
-      author
-    };
+    const book = new Book(new Date().getTime(), title, author);
     if (localStorage.getItem('booksData') !== null) {
       tempBooks = JSON.parse(localStorage.getItem('booksData'));
       tempBooks.push(book);
@@ -28,42 +25,24 @@ class Book {
     }
   };
 
-  displayBooks = () => {
-    const booksData = localStorage.getItem('booksData');
-    const convertedBooks = JSON.parse(booksData);
-    document.getElementById('bookstatus').innerHTML = 'No books added';
-    if (convertedBooks && convertedBooks.length === 0) {
-      document.getElementById('bookstatus').innerHTML = 'No books added';
-    } else {
-      document.getElementById('bookstatus').innerHTML = '';
-      let html = '';
-      convertedBooks.forEach((book) => {
-        html += `<article>
-       <h2>${book.title}</h2>
-       <h2>${book.author}</h2>
-       <button data-book-id = "${book.id}" id = "remove-button" onclick="removeBook(${book.id})">Remove</button>
-      
-     </article></br><hr>`;
-      });
-      document.getElementById('bookslist').innerHTML = html;
-    }
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  removeBook(id) {
-    const booksData = localStorage.getItem('booksData');
-    const convertedBooks = JSON.parse(booksData);
-    const remainingBooks = convertedBooks.filter((book) => book.id !== id);
-    const removedBooks = JSON.stringify(remainingBooks);
-    localStorage.setItem('booksData', removedBooks);
-    window.location.reload();
-  }
 }
 
+// eslint-disable-next-line no-unused-vars
+const removeBook = (id) => {
+  const booksData = localStorage.getItem('booksData');
+  const convertedBooks = JSON.parse(booksData);
+  const remainingBooks = convertedBooks.filter((book) => book.id !== id);
+  const removedBooks = JSON.stringify(remainingBooks);
+  localStorage.setItem('booksData', removedBooks);
+  window.location.reload();
+};
 
-document.getElementById('addbook').addEventListener('click', addBook);
+document.getElementById('addbook').addEventListener('click', () => {
+  const book = new Book();
+  book.addBook();
+});
 
-
-
-
-window.addEventListener('load', displayBooks);
+window.addEventListener('load', () => {
+  const book = new Book();
+  book.displayBooks();
+});
